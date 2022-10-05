bb4a0ef63ee34ce4d0547269dcc99407d3a4356d
mariadesanchez <mariade_snchz@hotmail.com>
2022-10-05 19:28:15 -0300

    Number prefijo II

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const $resultados = document.querySelector("#resultado");
-	Quagga.init({
-		inputStream: {
-			constraints: {
-				width: 1920,
-				height: 1080,
-			},
-			name: "Live",
-			type: "LiveStream",
-			target: document.querySelector('#contenedor'), // Pasar el elemento del DOM
-		},
-		decoder: {
-			readers: ["ean_reader"]
-		}
-	}, function (err) {
-		if (err) {
-			console.log(err);
-			return
-		}
-		console.log("Iniciado correctamente");
-		Quagga.start();
-	});
-
-	Quagga.onDetected((data) => {
-		$resultados.textContent = data.codeResult.code;
-		// Imprimimos todo el data para que puedas depurar
-		console.log(data);
-	});
-});
 function isMobile() {
     if (sessionStorage.desktop)
         return false;
@@ -43,7 +13,26 @@ const formulario = document.querySelector('#formulario');
 const buttonSubmit = document.querySelector('#submit');
 const urlDesktop = 'https://web.whatsapp.com/';
 const urlMobile = 'whatsapp://';
-const telefono = '+5492213602683';
+const telefono = '542213602683';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let apellidos = document.querySelector('#apellidos').value
+        let email = document.querySelector('#email').value
+        let mensaje = 'send?phone=' + telefono + '&text=*_Formulario Easy App CODE_*%0A*¿Cual es tu nombre?*%0A' + nombre + '%0A*¿Cuáles son tus apellidos?*%0A' + apellidos + '%0A*¿Cuál es tu correo electrónico?*%0A' + email + ''
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
