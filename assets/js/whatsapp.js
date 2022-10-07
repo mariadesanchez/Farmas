b4be4026508a3db2dcd27027709abdae1d95caf8
mariadesanchez <mariade_snchz@hotmail.com>
2022-10-06 23:18:46 -0300

    Index new

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -19,7 +19,7 @@ $form.addEventListener('submit', (event) => {
         let name = document.querySelector('#name').value
         let lastname = document.querySelector('#lastname').value
         let email = document.querySelector('#email').value
-        let message = 'send?phone=' + phone + '&text=*_Quisiera pasar la siguiente receta de IOMA MEPPES_*%0A*Contacto*%0A%0A*Nombre*%0A' + name + '%0A*Apellido*%0A' + lastname + '%0A*e-mail*%0A' + email + ''
+        let message = 'send?phone=' + phone + '&text=*_Lucky_*%0A*Contacto*%0A%0A*¿Cual es tu nombre?*%0A' + name + '%0A*¿Cuáles son tus apellidos?*%0A' + lastname + '%0A*¿Cuál es tu correo?*%0A' + email + ''
 
 
         if (isMobile()) {
